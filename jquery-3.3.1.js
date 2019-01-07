3722af829c5fd08191cac013b375f518d115f7ca
iyerikuzwe <iyerikuzweregine19@gmail.com>
2019-01-07 15:11:52 +0200

    add all changes

--- jquery-3.3.1.js
+++ jquery-3.3.1.js
@@ -137,7 +137,7 @@
   aria-expanded="false"
 >
   <div class="position-relative">
-    <!-- '"` --><!-- </textarea></xmp> --></option></form><form class="js-site-search-form" data-scope-type="Repository" data-scope-id="162687688" data-scoped-search-url="/CarineIzere/QUIZ-BOARD-PROJECT/search" data-unscoped-search-url="/search" action="/CarineIzere/QUIZ-BOARD-PROJECT/search" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="&#x2713;" />
+    <!-- '"` --><!-- </textarea></xmp> --></option></form><form class="js-site-search-form" data-scope-type="Repository" data-scope-id="162687688" data-scoped-search-url="/iyerikuzwe/QUIZ-BOARD-PROJECT/search" data-unscoped-search-url="/search" action="/CarineIzere/QUIZ-BOARD-PROJECT/search" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="&#x2713;" />
       <label class="form-control header-search-wrapper header-search-wrapper-jump-to position-relative d-flex flex-justify-between flex-items-center js-chromeless-input-container">
         <input type="text"
           class="form-control header-search-input jump-to-field js-jump-to-field js-site-search-focus js-site-search-field is-clearable"
@@ -550,7 +550,7 @@
 
       <h1 class="public ">
   <svg class="octicon octicon-repo" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"/></svg>
-  <span class="author" itemprop="author"><a class="url fn" rel="author" data-hovercard-type="user" data-hovercard-url="/hovercards?user_id=45596468" data-octo-click="hovercard-link-click" data-octo-dimensions="link_type:self" href="/CarineIzere">CarineIzere</a></span><!--
+  <span class="author" itemprop="author"><a class="url fn" rel="author" data-hovercard-type="user" data-hovercard-url="/hovercards?user_id=45596468" data-octo-click="hovercard-link-click" data-octo-dimensions="link_type:self" href="/CarineIzere">iyerikuzwe</a></span><!--
 --><span class="path-divider">/</span><!--
 --><strong itemprop="name"><a data-pjax="#js-repo-pjax-container" href="/iyerikuzwe/QUIZ-BOARD-PROJECT">QUIZ-BOARD-PROJECT</a></strong>
 
